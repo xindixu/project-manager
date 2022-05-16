29dfd5f854c378f3f5118b2ed33a24b03799b938
Xindi Xu <xindixu0@gmail.com>
2022-05-15 22:49:24 -0400

    feat: render form

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,28 @@
-// Code goes here!
+class ProjectInput {
+  // Goal: Render form template
+
+  templateEl: HTMLTemplateElement
+
+  containerEl: HTMLDivElement
+
+  element: HTMLFormElement
+
+  constructor() {
+    this.templateEl = document.getElementById(
+      "project-input",
+    )! as HTMLTemplateElement
+
+    this.containerEl = document.getElementById("app")! as HTMLDivElement
+
+    const importedNode = document.importNode(this.templateEl.content, true)
+    this.element = importedNode.firstElementChild as HTMLFormElement
+
+    this.attach()
+  }
+
+  private attach() {
+    this.containerEl.insertAdjacentElement("afterbegin", this.element)
+  }
+}
+
+const projectInput = new ProjectInput()
